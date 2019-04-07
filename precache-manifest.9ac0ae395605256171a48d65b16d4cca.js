df38e63d32a5ca80b636ca6d9c1a387ac5555c29
NielsGregers <niels@jumpto365.com>
2019-04-07 08:37:49 +0200

    Updates

---
+++ precache-manifest.9ac0ae395605256171a48d65b16d4cca.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "96814bf83b0326d5ba07d2f707215ed8",
+    "url": "/shop/static/media/McCoolGrey.96814bf8.jpg"
+  },
+  {
+    "revision": "0631a267a240781c3c25",
+    "url": "/shop/static/css/main.3a4af131.chunk.css"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/shop/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "21d2e3e3d2ed756319d4",
+    "url": "/shop/static/js/2.b1ef5a99.chunk.js"
+  },
+  {
+    "revision": "9258ba321078f889201937be83c9ac6b",
+    "url": "/shop/static/media/logo.9258ba32.png"
+  },
+  {
+    "revision": "237f3501b3768ca121c0dcec43daf6c3",
+    "url": "/shop/static/media/rock-n-roll-grey-front.237f3501.jpg"
+  },
+  {
+    "revision": "b1bfd3b329e1a6087748381b0baaead5",
+    "url": "/shop/static/media/rock-n-roll-white-front.b1bfd3b3.jpg"
+  },
+  {
+    "revision": "bbcdc7c297d259fc062213ba5b490e79",
+    "url": "/shop/static/media/fy-white-front.bbcdc7c2.jpg"
+  },
+  {
+    "revision": "f2aa3a26f22ca7f75c8842aa8611111e",
+    "url": "/shop/static/media/BagsideSorttshirt.f2aa3a26.jpg"
+  },
+  {
+    "revision": "5b91c9af0f967f20562d09628e8dedea",
+    "url": "/shop/static/media/Bagsideafgreyshirt.5b91c9af.jpg"
+  },
+  {
+    "revision": "23190fe35a59e876a412a3704712a10e",
+    "url": "/shop/static/media/Bagsidegulhat.23190fe3.jpg"
+  },
+  {
+    "revision": "fe4ef0714e2559039c33a9389a9088f6",
+    "url": "/shop/static/media/Bagsideredhat.fe4ef071.jpg"
+  },
+  {
+    "revision": "528612583358c54580e58f285e60895e",
+    "url": "/shop/static/media/BoisssGrey.52861258.jpg"
+  },
+  {
+    "revision": "a64d8b546c49c6d0134e03e72cb201fb",
+    "url": "/shop/static/media/BoisssHvid.a64d8b54.jpg"
+  },
+  {
+    "revision": "ee2ab74ffb59a2eb069c074195a52142",
+    "url": "/shop/static/media/Boisssort.ee2ab74f.jpg"
+  },
+  {
+    "revision": "1d29ec0706b6993b0581db5b9154105e",
+    "url": "/shop/static/media/FuckYou-grey.1d29ec07.jpg"
+  },
+  {
+    "revision": "93a82dbaaee6f932d183bb354a80b576",
+    "url": "/shop/static/media/Fuckyou-sort.93a82dba.jpg"
+  },
+  {
+    "revision": "0fce017a38ecb6564933386f0dd18707",
+    "url": "/shop/static/media/Gulhat.0fce017a.jpg"
+  },
+  {
+    "revision": "477366cf524005836906476fbd8a1201",
+    "url": "/shop/static/media/Hvidfuckyou-.477366cf.jpg"
+  },
+  {
+    "revision": "82ae00ca801c4e7b7b666c17a7480914",
+    "url": "/shop/static/media/Hvidtshirtbagside.82ae00ca.jpg"
+  },
+  {
+    "revision": "0631a267a240781c3c25",
+    "url": "/shop/static/js/main.ec108d9a.chunk.js"
+  },
+  {
+    "revision": "42d35447441ce39b7cb43f1cec6b2f9b",
+    "url": "/shop/static/media/McCoolHvid.42d35447.jpg"
+  },
+  {
+    "revision": "965eebbdc9eefab9681e44895c25e986",
+    "url": "/shop/static/media/McCoolsort.965eebbd.jpg"
+  },
+  {
+    "revision": "43ad1e68c212de176250ea2a4f82e4e0",
+    "url": "/shop/static/media/Redhat.43ad1e68.jpg"
+  },
+  {
+    "revision": "d4b732d62a63331876ed60a8163bf450",
+    "url": "/shop/static/media/grey.back.d4b732d6.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/shop/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/shop/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/shop/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/shop/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/shop/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/shop/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/shop/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/shop/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/shop/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/shop/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/shop/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/shop/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/shop/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/shop/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/shop/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "21d2e3e3d2ed756319d4",
+    "url": "/shop/static/css/2.3f9609ed.chunk.css"
+  },
+  {
+    "revision": "9c904b1fcad5b33a16dac974250b57e3",
+    "url": "/shop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7818810d59d25f78428bad3feb379938.js"
+  "/shop/precache-manifest.9ac0ae395605256171a48d65b16d4cca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shop/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
